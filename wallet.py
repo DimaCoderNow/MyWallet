7f40f6c78d6b45a7581228eea21e1f6a3916839a
Dmitriy Zamarekhin <zam1986@yandex.ru>
2024-05-05 11:46:34 +0200

    Initial commit

---
+++ wallet.py
@@ -0,0 +1,62 @@
+class Commands:
+    """
+        Обработка команд
+    """
+
+    def __init__(self):
+        ...
+
+
+class Wallet:
+    """
+        Кошелек пользователя
+    """
+    def __init__(self):
+        ...
+
+
+class DataBase:
+    """
+        Хранение данных
+    """
+
+    def __init__(self):
+        ...
+
+
+class Income:
+    """
+        Учет расходов
+    """
+
+    def __init__(self):
+        ...
+
+
+class User:
+    """
+        Пользователь кошелька
+    """
+
+    def __init__(self):
+        ...
+
+
+class Expenses:
+    """
+        Учет доходов
+    """
+    def __init__(self):
+        ...
+
+
+class FindItem:
+    """
+        Поиск записей по категории, дате или сумме
+    """
+    def __init__(self):
+        ...
+
+
+if __name__ == '__main__':
+    app = Commands()
