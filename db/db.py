10c2a00574b535105e5677e8179e8f5e19d799e8
Dmitriy Zamarekhin <zam1986@yandex.ru>
2024-05-05 15:25:15 +0200

    Added setter and getter for db

--- db/db.py
+++ db/db.py
@@ -1,7 +1,38 @@
+import json
+from typing import List, Dict
+
+
 class DataBase:
     """
         Хранение данных
     """
+    empty_data: Dict = {
+        "expenses": [],
+        "income": [],
+        "balance": 0
+    }
 
     def __init__(self):
-        ...
+        self._data = self._get_data()
+
+    @property
+    def data(self) -> Dict:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: Dict):
+        self._data = new_data
+        self._save_data()
+
+    def _get_data(self) -> Dict:
+        try:
+            with open('db/data.json', 'r', encoding='utf-8') as file_data:
+                return json.load(file_data)
+        except FileNotFoundError:
+            with open('db/data.json', 'w', encoding='utf-8') as file_data:
+                json.dump(self.empty_data, file_data, indent=4)
+                return self.empty_data
+
+    def _save_data(self):
+        with open('db/data.json', 'w', encoding='utf-8') as file_data:
+            json.dump(self._data, file_data, indent=4)
